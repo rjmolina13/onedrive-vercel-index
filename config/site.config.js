5814c8b29f6cf17129b6d0540868effb6a888773
Ronanj Molina <rj.molina13@gmail.com>
2023-04-04 13:33:03 +0800

    New config

--- config/site.config.js
+++ config/site.config.js
@@ -38,23 +38,31 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SpencerWoo.',
+    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Site by RubyJ. ❤',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['test'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:spencer.wushangbo@gmail.com',
+  email: 'mailto:rj.molina13@gmail.com',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
-    },
+    name: 'GitHub',
+    link: 'https://github.com/rjmolina13',
+},
+{
+    name: 'Telegram',
+    link: 'https://t.me/rjmolina13',
+},
+{
+    name: 'Twitter',
+    link: 'https://twitter.com/rjmolina13',
+},
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
